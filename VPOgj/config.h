d4d357b62893a41d28d7fd8532dd768e388bc131
David Mansueto <david.mansueto@proton.me>
2025-02-22 17:48:25 +1030

    Updates

--- VPOgj/config.h
+++ VPOgj/config.h
@@ -1,7 +1,10 @@
 /*
   Set any config.h overrides for your specific keymap here.
-  See config.h options at https://docs.qmk.fm/#/config_options?id=the-configh-file
+  See config.h options at
+  https://docs.qmk.fm/#/config_options?id=the-configh-file
 */
+
+// ZSA ORYX-DEFINED
 
 #define USB_SUSPEND_WAKEUP_DELAY 0
 #define SERIAL_NUMBER "VPOgj/zLoZp4"
@@ -10,13 +13,17 @@
 
 #define RGB_MATRIX_STARTUP_SPD 60
 
-// DAV0S
+// DAV0S CONFIGURATION
 
-// Slow down macros to improve detection reliability.
-#define TAP_CODE_DELAY 5
+// MACRO BEHAVIOUR
+#define TAP_CODE_DELAY 5 // (s) Slow down macro playback improves detection.
 
-// Tap-hold configuration for home row mods.
-#define TAPPING_TERM 170
-#define TAPPING_TERM_PER_KEY
-#define PERMISSIVE_HOLD
-#define QUICK_TAP_TERM_PER_KEY
+// TAP-HOLD BEHAVIOUR
+#define TAPPING_TERM 222     // (ms) Maximum term a key press can be a 'tap'.
+#define TAPPING_TERM_PER_KEY // Not sure where to configure though...
+#define PERMISSIVE_HOLD // Ignores TAPPING_TERM and considers any length of key
+// press to be a 'hold' if another key is pressed and released while the first
+// is still pressed.
+#define QUICK_TAP_T RM_PER_KEY
+// Note: Also using Getreuer Achordion:
+// https://getreuer.info/posts/keyboards/achordion/index.html
